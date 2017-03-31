4808bd367f656f60f824ab089cc38dca761caa42
Collin Dreher <collin.dreher@gmail.com>
2017-03-31 14:07:06 -0400

    Create thankyou.php

---
+++ lab10/thankyou.php
@@ -0,0 +1,17 @@
+<!--Collin Dreher-->
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Thank You!</title>
+</head>
+
+<body>
+	
+	 <h2>Thank you,</h2>
+	 <p>Your details are being processed.</p>
+	 
+	 <p><a href = "http://cdreher26.x10host.com/CS0334/content.php">Go back to submission page</a></p>
+</body>
+
+</html>
