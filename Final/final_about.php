dcb5ccbba4cec4e95b3a3a557968edd5c64d8fcf
Collin Dreher <collin.dreher@gmail.com>
2017-04-18 17:51:01 -0400

    Create final_about.php

---
+++ Final/final_about.php
@@ -0,0 +1,39 @@
+<!--Collin Dreher-->
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About</title>
+	<link href="aboutFINAL1.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<style>body{background-color: #144f2d; color:#42f48f;}</style>
+	<div style="background-color: #42f48f; color: #000000;" id = "top"></div>
+	<h3>About my Golf Shop</h3>
+	<p>Welcome to my golf shop! I am pleased to have your business. Just a little bit about me. I was born and raised in Hershey, PA. In high school I started to play golf competetively. I went on to play golf at Pitt, and after school I became very successful in the field of Software Engineering. After I retired, I decided what better way to spend my money than making a golf course. The course was built in Pittsburgh in 2055. I then decided to open up a separate golf shop online, where you are today. We have all of the newest golf equipment and technology.
+	</p>
+	
+	<div id="drivers">
+		<img height="300" width="300" src="final_pics/drivers.jpg" alt="Drivers">
+		<a href = "http://www.golfgalaxy.com/f/mens-golf-drivers"><button type="button">Drivers</button></a>
+	</div>
+	<div id="irons">
+		<img height="300" width="300" src="final_pics/irons.jpg" alt="Irons">
+		<a href="http://www.golfgalaxy.com/f/mens-golf-irons"><button type="button">Irons</button></a>
+	</div>
+	<div id="shoes">
+		<img height="300" width="300" src="final_pics/shoes.jpg" alt="Footwear">
+		<a href="http://www.golfgalaxy.com/c/mens-golf-shoes"><button type="button">Footwear</button></a>
+	</div>
+	<div id="golfballs">
+		<img height="300" width="300" src="final_pics/golf balls.jpg" alt="Golf Balls">
+		<a href="http://www.golfgalaxy.com/d/golf-balls"><button type="button">Golf Balls</button></a>
+	</div>
+	<div id="wedges">
+		<img height="300" width="300" src="final_pics/wedges.jpg" alt="Wedges">
+		<a href="http://www.golfgalaxy.com/f/mens-golf-wedges"><button type="button">Wedges</button>
+	</div>
+	
+	<div><p><br><a href="http://cdreher26.x10host.com/CS0334/final_project.html"><button type="button">Go back to main page</button></a></p></div>
+
+</body>
+</html>
