b7a3170903602a7557de9c026f8902e400408fa8
Collin Dreher <collin.dreher@gmail.com>
2017-04-18 17:51:20 -0400

    Create final_contact.php

---
+++ Final/final_contact.php
@@ -0,0 +1,23 @@
+<!--Collin Dreher-->
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contact me</title>
+</head>
+<body>
+<style>body{background-color: #144f2d; color:#42f48f;}</style>
+	<h1>Contact Me:</h1>
+	<h3>Phone: (717)805-8885     Email: collindreher@aol.com
+	<div>
+		<a href="https://twitter.com/collin_dreher26"><img height="100" width="100" src="final_pics/twitter.png"></a>
+		<a href="https://www.facebook.com/collin.dreher"><img height="100" width="100" src="final_pics/facebook.png"></a>
+	
+		<a href="https://www.instagram.com/collin.dreher/?hl=en"><img height="100" width="100" src="final_pics/insta.png"></a>
+	
+		<a href="https://www.linkedin.com/in/collin-dreher-917182133/"><img height="100" width="100" src="final_pics/linkedin.png"></a>
+	
+		<a href="https://github.com/cdreher"><img height="100" width="100" src="final_pics/github.png"></a>
+	</div>
+	<p><a href = "http://cdreher26.x10host.com/CS0334/final_project.html"><button type="button">Go back to main page</button></a></p>
+
+</html>
